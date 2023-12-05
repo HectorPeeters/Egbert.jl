abd002b11bdb1239056a8cf0da7e9658c67997af
Hector Peeters <hector.peeters@gmail.com>
2023-12-05 12:21:00 +0100

    Use new build_opt_pipeline function

--- src/interpreter.jl
+++ src/interpreter.jl
@@ -27,6 +27,24 @@ CC.OptimizationParams(interp::CustomInterpreter) = interp.opt_params
 CC.get_world_counter(interp::CustomInterpreter) = interp.world
 CC.get_inference_cache(interp::CustomInterpreter) = interp.inf_cache
 CC.code_cache(interp::CustomInterpreter) = WorldView(interp.code_cache, interp.world)
+function CC.build_opt_pipeline(::CustomInterpreter)
+    pm = CC.PassManager()
+
+    CC.register_pass!(pm, "slot2reg", CC.slot2reg)
+    CC.register_pass!(pm, "compact 1", (ir, ci, sv) -> CC.compact!(ir))
+    CC.register_pass!(pm, "Inlining", (ir, ci, sv) -> CC.ssa_inlining_pass!(ir, sv.inlining, ci.propagate_inbounds))
+    CC.register_pass!(pm, "compact 2", (ir, ci, sv) -> CC.compact!(ir))
+    CC.register_pass!(pm, "SROA", (ir, ci, sv) -> CC.sroa_pass!(ir, sv.inlining))
+    CC.register_pass!(pm, "ADCE", (ir, ci, sv) -> CC.adce_pass!(ir, sv.inlining))
+    CC.register_pass!(pm, "compact 2", (ir, ci, sv) -> CC.compact!(ir, true))
+    CC.register_pass!(pm, "verify 3", function (ir, ci, sv)
+        CC.verify_ir(ir, true, false, CC.optimizer_lattice(sv.inlining.interp))
+        CC.verify_linetable(ir.linetable)
+        return ir
+    end)
+
+    return pm
+end
 
 CC.lock_mi_inference(::CustomInterpreter, ::MethodInstance) = nothing
 CC.unlock_mi_inference(::CustomInterpreter, ::MethodInstance) = nothing
