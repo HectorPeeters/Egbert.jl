b2caaa3c7fb7bc2f558a0f26fda61cf25852847f
Hector Peeters <hector.peeters@gmail.com>
2024-02-25 15:26:35 +0100

    Add rewritetarget macro
    This wraps a function in a Base.compilerbarrier

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,5 @@
-using GpuOptim: @custom
-using Test
-
-@noinline intrinsic(@nospecialize(val)) = Base.compilerbarrier(:const, val)
-
-macro rewritetarget(func::Expr)
-    func_name = func.args[begin].args[begin]
-    args = func.args[begin].args[2:end]
-
-    return esc(quote
-        function $func_name($(args...))
-            return Base.compilerbarrier(:const, $(func)($(args...)))
-        end
-    end)
-end
-
-function times_two(a)
-    a + a
-end
+using GpuOptim: @custom, @rewritetarget
+using Test: @testset, @test
 
 struct CustomList
     data::Vector{Int}
@@ -24,18 +7,19 @@ end
 
 function add_impl(a::CustomList, b::CustomList)
     CustomList(a.data .+ b.data)
+    return error("This is broken to make sure the test rewrites this.")
 end
 
-@noinline function add(a::CustomList, b::CustomList)
+@rewritetarget function add(a::CustomList, b::CustomList)::CustomList
     add_impl(a, b)
 end
 
-function mul(a::CustomList, b::CustomList)
-    CustomList(a.data .* b.data)
+@rewritetarget function mul(a::CustomList, b::CustomList)::CustomList
+    return CustomList(a.data .* b.data)
 end
 
 function add_mul(a::CustomList, b::CustomList, c::CustomList)
-    CustomList(a.data .+ b.data .* c.data)
+    return CustomList(a.data .+ b.data .* c.data)
 end
 
 function optimizetarget(a, b, c)
@@ -47,6 +31,5 @@ B = CustomList([4, 5, 6])
 C = CustomList([7, 8, 9])
 
 @testset "GpuOptim.jl" begin
-    # @test (@custom times_two(13)) == 26
     @test (@custom optimizetarget(A, B, C)).data == [29, 42, 57]
 end
