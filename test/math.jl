550e6727826d9b8bd924edc172fd844803f3436b
Hector Peeters <hector.peeters@gmail.com>
2024-05-07 18:32:38 +0200

    Fix math identity test

--- test/math.jl
+++ test/math.jl
@@ -3,37 +3,35 @@ using Test: @testset, @test
 using Metatheory
 using BenchmarkTools: @benchmark
 
-struct MyInteger
-    data::Integer
+global crash_mul = false
 
-    @rewritetarget function MyInteger(data::Integer)::MyInteger
-        new(data)
+@rewritetarget function add(a::Integer, b::Integer)::Integer
+    return a + b
+end
+
+@rewritetarget function sub(a::Integer, b::Integer)::Integer
+    return a - b
+end
+
+@rewritetarget function mul(a::Integer, b::Integer)::Integer
+    if crash_mul
+        error("Should be replaced by optimization")
     end
+    return a * b
 end
 
-@rewritetarget function add(a::MyInteger, b::MyInteger)::MyInteger
-    return MyInteger(a.data + b.data)
+@rewritetarget function pow(a::Integer)::Integer
+    return a * a
 end
 
-@rewritetarget function sub(a::MyInteger, b::MyInteger)::MyInteger
-    return MyInteger(a.data - b.data)
-end
-
-@rewritetarget function mul(a::MyInteger, b::MyInteger)::MyInteger
-    return MyInteger(a.data * b.data)
-end
-
-@rewritetarget function pow(a::MyInteger)::MyInteger
-    return MyInteger(a.data * a.data)
-end
-
-function tooptimize(a::MyInteger, b::MyInteger)
-    return add(pow(a), add(mul(MyInteger(2), mul(a, b)), pow(b)))
+function tooptimize(a::Integer, b::Integer)
+    return add(pow(a), add(mul(Integer(2), mul(a, b)), pow(b)))
 end
 
 rules = @theory a b c begin
     pow(a) == mul(a, a)
 
+    add(a, a) == mul(a, 2)
     add(a, b) == add(b, a)
     add(a, add(b, c)) == add(add(a, b), c)
     add(a, 0) == a
@@ -42,17 +40,21 @@ rules = @theory a b c begin
     mul(a, mul(b, c)) == mul(mul(a, b), c)
     mul(a, 1) == a
 
-    pow(add(a, b)) == add(pow(a), add(mul(2, mul(a, b)), pow(b)))
-    pow(sub(a, b)) == add(sub(pow(a), mul(2, mul(a, b))), pow(b))
+    add(pow(a), add(mul(2, mul(a, b)), pow(b))) == pow(add(a, b))
+    add(sub(pow(a), mul(Integer(2), mul(a, b))), pow(b)) == pow(sub(a, b))
 
     # Constant folding rules
-    add(a::MyInteger, b::MyInteger) => add(a, b)
-    sub(a::MyInteger, b::MyInteger) => sub(a, b)
-    mul(a::MyInteger, b::MyInteger) => mul(a, b)
-    pow(a::MyInteger) => pow(a)
+    add(a::Integer, b::Integer) => a + b
+    sub(a::Integer, b::Integer) => a - b
+    mul(a::Integer, b::Integer) => a * b
+    pow(a::Integer) => a * a
 end
 
 @testset "Identities" begin
-    @test tooptimize(MyInteger(12), MyInteger(13)) == MyInteger(625)
-    @test (@custom Options() rules tooptimize(MyInteger(12), MyInteger(13))) == MyInteger(625)
+    @test tooptimize(12, 13) == 625
+
+    @test begin
+        global crash_mul = true
+        (@custom Options() rules tooptimize(12, 13)) == 625
+    end
 end
