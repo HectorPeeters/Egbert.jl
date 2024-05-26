3ec14a48260011d208331022157b3eeebb7ac89c
Hector Peeters <hector.peeters@gmail.com>
2024-05-26 14:29:53 +0200

    Cleanup rewrite rules in math_min_timilimit.jl

--- benches/math_min_timelimit.jl
+++ benches/math_min_timelimit.jl
@@ -3,25 +3,11 @@ using Test: @testset, @test
 using Metatheory
 using BenchmarkTools: @benchmark
 
-@rewritetarget function add(a::Integer, b::Integer)::Integer
-    return a + b
-end
-
-@rewritetarget function sub(a::Integer, b::Integer)::Integer
-    return a - b
-end
-
-@rewritetarget function mul(a::Integer, b::Integer)::Integer
-    return a * b
-end
-
-@rewritetarget function div(a::Integer, b::Integer)::Integer
-    return a / b
-end
-
-@rewritetarget function pow(a::Integer, b::Integer)::Integer
-    return a^b
-end
+@rewritetarget add(a::Integer, b::Integer)::Integer = a + b
+@rewritetarget sub(a::Integer, b::Integer)::Integer = a - b
+@rewritetarget mul(a::Integer, b::Integer)::Integer = a * b
+@rewritetarget div(a::Integer, b::Integer)::Intege = a / b
+@rewritetarget pow(a::Integer, b::Integer)::Integer = a^b
 
 function tooptimize(a::Integer, b::Integer)
     asquared = pow(a, 2)
@@ -34,14 +20,15 @@ end
 
 @test (@custom Options() math_identities tooptimize(12, 13)) == 625
 
-for i in 25:35
+for i in 5:35
     println("Iteration ", i, " limit ", i * 100_000, " ns")
     params = SaturationParams(timelimit=i * 100_000)
     options = Options(
         saturation_params=params,
         enable_caching=false,
         dont_run=true,
-        log_ir=true
+        log_ir=true,
+        print_sat_info=true
     )
 
     @custom options math_identities tooptimize(12, 13)
