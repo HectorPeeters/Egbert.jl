bdef2a9727f9ba7dcab0a9dd307b1ba7976258ca
Hector Peeters <hector.peeters@gmail.com>
2024-05-26 14:27:08 +0200

    Clean up gemm benchmark

--- benches/gemm.jl
+++ benches/gemm.jl
@@ -1,6 +1,6 @@
 using GpuOptim: @custom, @rewritetarget_ef, Options
 using Test: @testset, @test
-using LinearAlgebra: sum, transpose, tr
+using LinearAlgebra: sum, transpose
 using LinearAlgebra.BLAS: gemm, gemm!
 using BenchmarkTools
 using Metatheory
@@ -12,53 +12,18 @@ using Statistics
 @rewritetarget_ef mul(A::Matrix, B::Matrix)::Matrix = A * B
 @rewritetarget_ef transp(A::Matrix)::Matrix = transpose(A)
 
-@inline mul_optimized(A::Matrix, B::Matrix, ta, tb)::Matrix = gemm(ta, A + B, A, B)
+@inline mul_optimized(A::Matrix, B::Matrix, ta, tb)::Matrix = gemm(ta, tb, A, B)
 @rewritetarget_ef mul_optimizedNN(A::Matrix, B::Matrix)::Matrix = mul_optimized(A, B, 'N', 'N')
 @rewritetarget_ef mul_optimizedTN(A::Matrix, B::Matrix)::Matrix = mul_optimized(A, B, 'T', 'N')
 @rewritetarget_ef mul_optimizedNT(A::Matrix, B::Matrix)::Matrix = mul_optimized(A, B, 'N', 'T')
 @rewritetarget_ef mul_optimizedTT(A::Matrix, B::Matrix)::Matrix = mul_optimized(A, B, 'T', 'T')
 
-@inline function addmul_optimized(A::Matrix, B::Matrix, C::Matrix, ta, tb)::Matrix
-    D = deepcopy(C)
-    return Matrix(gemm!(ta, tb, 1.0, A, B, 1.0, D))
-end
-@rewritetarget_ef addmul_optimizedNN(A::Matrix, B::Matrix, C::Matrix)::Matrix = addmul_optimized(A, B, C, 'N', 'N')
-@rewritetarget_ef addmul_optimizedNT(A::Matrix, B::Matrix, C::Matrix)::Matrix = addmul_optimized(A, B, C, 'N', 'T')
-@rewritetarget_ef addmul_optimizedTN(A::Matrix, B::Matrix, C::Matrix)::Matrix = addmul_optimized(A, B, C, 'T', 'N')
-@rewritetarget_ef addmul_optimizedTT(A::Matrix, B::Matrix, C::Matrix)::Matrix = addmul_optimized(A, B, C, 'T', 'T')
-
 function tooptimize(A::Matrix, B::Matrix, C::Matrix)
-    return add(mul(transp(A), transp(B)), C)
+    return mul(transp(A), transp(B))
 end
 
 function baseline(A::Matrix, B::Matrix, C::Matrix)
-    return transpose(A) * transpose(B) + C
-end
-
-function init_layer(input_dim::Int, output_dim::Int)
-    W = randn(output_dim, input_dim)
-    B = randn(output_dim, 2012)
-    return W, B
-end
-
-@inline function forward_layer(I::Matrix, W::Matrix, B::Matrix)
-    return add(mul(I, transp(W)), transp(B))
-end
-
-function nn(I::Matrix, W1, B1, W2, B2, W3, B3)
-    hidden1_out = forward_layer(I, W1, B1)
-    hidden2_out = forward_layer(hidden1_out, W2, B2)
-    return forward_layer(hidden2_out, W3, B3)
-end
-
-@inline function forward_layer_baseline(I::Matrix, W::Matrix, B::Matrix)
-    return I * transpose(W) + transpose(B)
-end
-
-function nn_baseline(I::Matrix, W1, B1, W2, B2, W3, B3)
-    hidden1_out = forward_layer_baseline(I, W1, B1)
-    hidden2_out = forward_layer_baseline(hidden1_out, W2, B2)
-    return forward_layer_baseline(hidden2_out, W3, B3)
+    return transpose(A) * transpose(B)
 end
 
 rules = @theory A B C begin
@@ -82,34 +47,14 @@ rules = @theory A B C begin
     mul(transp(A), transp(B)) --> mul_optimizedTT(A, B)
 end
 
-input_dim = 4024
-hidden1_dim = 8024
-hidden2_dim = 8024
-output_dim = 2012
-
-I = randn(2012, input_dim)
-
-W1, B1 = init_layer(input_dim, hidden1_dim) 
-W2, B2 = init_layer(hidden1_dim, hidden2_dim) 
-W3, B3 = init_layer(hidden2_dim, output_dim) 
-
-t = @benchmark (@custom Options() rules nn($I, $W1, $B1, $W2, $B2, $W3, $B3))
-println(t)
-tslow = @benchmark nn_baseline($I, $W1, $B1, $W2, $B2, $W3, $B3)
-println(tslow)
-
 xs = Float64[]
 ys = Float64[]
 ys_slow = Float64[]
 
-xs = Float64[]
-ys = Float64[]
-ys_slow = Float64[]
-
-for i in range(1, stop=1500, length=2)
-    println("Running for n = ", i)
-
+for i in range(0, stop=2000, length=2)
     n = floor(Int, i)
+    println("Running for n = ", n)
+
     A = rand(n, n)
     B = rand(n, n)
     C = rand(n, n)
